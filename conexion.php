2dd49fc48c163bce2a0ae628c6df73e7f311be01
Una-planta-dev <emersongirongarcia12@gmail.com>
2026-09-05 13:16:21 -0600

    Limpiar conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 $host = "localhost";
 $dbname = "proyecto_expoferia";
@@ -12,19 +11,4 @@ echo "Conexion exitosa a la base de datos";
     echo "ocurrio un error al conectar :(: " . $e->getMessage();
 
 }
-=======
-<?php 
-$host = "localhost";
-$dbname = "proyecto_expoferia";
-$username = "root";
-$password = "";
-
-try {$conexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "Conexion exitosa a la base de datos";
-}catch (PDOException $e) {
-    echo "ocurrio un error al conectar :(: " . $e->getMessage();
-
-}
->>>>>>> f7028d993f5ab001964b9e75507ad411aa3842f6
 ?>
