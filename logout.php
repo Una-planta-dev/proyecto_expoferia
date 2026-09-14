209408337dcac54f4887cc88c2d0b09d259fac5a
levirodriguez01 <levirodriguezumana@gmail.com>
2026-09-13 22:08:01 -0600

    boton de logout funcional

--- logout.php
+++ logout.php
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 <?php
 session_start();
-session_destroy();
-header("Location: login.php");
-exit();
-=======
 session_unset();
 session_destroy();
 header("Location: login.php");
->>>>>>> b4d6fbe7badd2433dbdbb3553ffc627837a984e2
+exit();
+?>
 
